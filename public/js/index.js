11043b887b38c79a40cadbebbe24bfd5624ef853
cris-gar <cristian.garrido@ceinf.cl>
2014-06-03 21:17:52 -0400

    los ramos mostrados en la pagina principal redireccion y muestra a que ramo pertenece

--- public/js/index.js
+++ public/js/index.js
@@ -8,7 +8,7 @@ if (localStorage.getItem('cantidadSemestre')) {
         car = localStorage.getItem(i);
         car = JSON.parse(car);
         $('#malla').append('<tr id="semestre' + i + '"></tr>');
-        columna1 = $('#semestre' + i).append('<td class="success"><input type="button" id="ramo" class="btn btn-success btn-sm" value="' + car.Ramo1 + '"></td><td class="success"><input type="button" id="ramo" class="btn btn-success btn-sm" value="' + car.Ramo2 + '"></td><td class="success"><input type="button" id="ramo" class="btn btn-success btn-sm" value="' + car.Ramo3 + '"></td><td class="success"><input type="button" id="ramo" class="btn btn-success btn-sm" value="' + car.Ramo4 + '"></td><td class="success"><input type="button" id="ramo" class="btn btn-success btn-sm" value="' + car.Ramo5 + '"></td><td class="success"><input type="button" id="ramo" class="btn btn-success btn-sm" value="' + car.Ramo6 + '"></td>');
+        columna1 = $('#semestre' + i).append('<td class="success"><input type="button" id="ramo1" class="btn btn-success btn-sm" value="' + car.Ramo1 + '"></td><td class="success"><input type="button" id="ramo2" class="btn btn-success btn-sm" value="' + car.Ramo2 + '"></td><td class="success"><input type="button" id="ramo3" class="btn btn-success btn-sm" value="' + car.Ramo3 + '"></td><td class="success"><input type="button" id="ramo4" class="btn btn-success btn-sm" value="' + car.Ramo4 + '"></td><td class="success"><input type="button" id="ramo5" class="btn btn-success btn-sm" value="' + car.Ramo5 + '"></td><td class="success"><input type="button" id="ramo6" class="btn btn-success btn-sm" value="' + car.Ramo6 + '"></td>');
         //columna1.append(car.Ramo1);
         //columna1.append('<td>' + car.Ramo2 + '</td>');
     }
@@ -17,11 +17,36 @@ if (localStorage.getItem('cantidadSemestre')) {
 else {
     alert("todavia no ingresas tu malla");
 }
-var test = document.getElementById('ramo');
-test.addEventListener('click', function() {
-    mandar();
-});
 
-function mandar() {
-    window.location = "./Notas.html";
+
+for (i = 1; i <= 6; i++) {
+    var test = document.getElementById('ramo1');
+    test.addEventListener('click', function() {
+        mandar(test.value);
+    });
+    var test2 = document.getElementById('ramo2');
+    test2.addEventListener('click', function() {
+        mandar(test2.value);
+    });
+    var test3 = document.getElementById('ramo3');
+    test3.addEventListener('click', function() {
+        mandar(test3.value);
+    });
+    var test4 = document.getElementById('ramo4');
+    test4.addEventListener('click', function() {
+        mandar(test4.value);
+    });
+    var test5 = document.getElementById('ramo5');
+    test5.addEventListener('click', function() {
+        mandar(test5.value);
+    });
+    var test6 = document.getElementById('ramo6');
+    test6.addEventListener('click', function() {
+        mandar(test6.value);
+    });
+}
+
+function mandar(ramo) {
+    //alert(ramo);
+    window.location = "./Notas.html?carrera=" + ramo;
 }
---
+++ public/js/notas.js
@@ -0,0 +1,19 @@
+function leerGET() {
+    var cadGET = location.search.substr(1, location.search.length);
+    var arrGET = cadGET.split("&");
+    var asocGET = new Array();
+    var variable = "";
+    var valor = "";
+    for (i = 0; i < arrGET.length; i++) {
+        var aux = arrGET[i].split("=");
+        variable = aux[0];
+        valor = aux[1];
+        asocGET[variable] = valor;
+    }
+    return asocGET;
+}
+
+var paresVarValor = leerGET();
+for (obj in paresVarValor) {
+    document.write("'" + obj + "''vale<b>" + paresVarValor[obj] + "</b><br>");
+}
